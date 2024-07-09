6c7bb0c673cc34a8336fbd9d099f51fc9cc16f0a
Jay Rana <91007351+techrj786@users.noreply.github.com>
2024-07-09 22:04:12 +0530

    Create Problem_01.c

---
+++ Problem_01.c
@@ -0,0 +1,11 @@
+# Get a number from the user
+user_input = input("Enter a number: ")
+
+# Convert the input to an integer
+number = int(user_input)
+
+# Add 2 to the number
+result = number + 2
+
+# Print the result
+print("The result is:", result)
